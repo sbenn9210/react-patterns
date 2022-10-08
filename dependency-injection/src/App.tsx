c910f76ef38012ce66b47df0fa18f2182e27b037
Shawn Benny <shawn.benny@umbrage.com>
2022-10-08 10:59:21 -0500

    Add redux

--- dependency-injection/src/App.tsx
+++ dependency-injection/src/App.tsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import { useAppDispatch, useAppSelector } from "redux/hooks";
+import { amountAdded } from "redux/slices/counter.slice";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!!!</h1>;
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  function handleClick() {
+    dispatch(amountAdded(1));
+  }
+  return (
+    <div>
+      <button
+        className="border-2 border-red-500 border-solid rounded-md"
+        onClick={handleClick}
+      >
+        count is {count}
+      </button>
+    </div>
+  );
 }
 
 export default App;
